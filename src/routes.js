c3cefc41604459d77767899e24b7766c32b01bb4
rahmatsyifana12 <adean435@gmail.com>
2022-03-11 21:58:42 +0700

    Add post method for refresh access token

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,16 @@ router.post('/login', loginUserHandler);
 router.post('/register', addNewUser);
 router.post('/products/add', addNewProduct);
 router.post('/products/:productId', auth, addProductToCart);
-router.post('/refreshtoken/');
+router.post('/refreshtoken/', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader.split(' ')[1];
+
+    if (!refreshToken) {
+        return res.status(401).json({
+            status: 'fail',
+            message: 'Unauthorized error'
+        });
+    }
+});
 
 module.exports = router;
